c4ee5d739a9fb509b9923b71c4c025733f72ac64
jialiu232 <jialiu232@gmail.com>
2023-12-11 10:55:51 -0600

    modified css

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/liujia/Documents/projects/basic_BLAST_tutorial_2023Dec")
+setwd("~/Documents/workshops/Basic_BLAST_workshop_2023Dec")
 
 #render your sweet site. 
 rmarkdown::render_site()
